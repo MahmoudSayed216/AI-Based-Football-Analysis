25cb67e49d5e92785cd3c1513455f3326dd08ac5
MahmoudSayed216 <mahmoud_das@outlook.com>
2026-03-30 13:55:05 +0200

    testing train_grid_and_save_best_model.py on kaggle env

--- train_grid_and_save_best_model.py
+++ train_grid_and_save_best_model.py
@@ -22,33 +22,33 @@ current_best_map = -100
 
 
 def train_model(model_path, model_name):
+    global current_best_map
     print(f"Training {model_name}")
-    with SuppressOutput():
-        model = YOLO(model=model_path, task='detect', verbose=False)
-        model.train(data=DATASET_DIR,
-                    epochs=2, 
-                    imgsz=640, 
-                    batch=16, 
-                    verbose=False, 
-                    save=False,
-                    plots=False,
-                    save_period=-1,
-                    exist_ok=True,
-                    val=False)
+    model = YOLO(model=model_path, task='detect', verbose=False)
+    model.train(data=DATASET_DIR,
+                epochs=2, 
+                imgsz=640, 
+                batch=16, 
+                verbose=False, 
+                save=False,
+                plots=False,
+                save_period=-1,
+                exist_ok=True,
+                val=False)
 
 
-        val_map = model.metrics.box.map
-        print(f"map@50-95: {val_map}")
-        if val_map > current_best_map:
-            current_best_map = val_map
-            model.val(data=DATASET_DIR, plots=True)
-            # shutil.copytree(src, OUTPUT_DIR, dirs_exist_ok=True)
+    val_map = model.metrics.box.map
+    print(f"map@50-95: {val_map}")
+    if val_map > current_best_map:
+        current_best_map = val_map
+        model.val(data=DATASET_DIR, plots=True)
+        # shutil.copytree(src, OUTPUT_DIR, dirs_exist_ok=True)
             
 
-
-
-
     print("################################")
+
+
+
 
 
 
